99ef6dabf8e5f43aa3f3f8f76f8de22b8296f1e6
Raline Silva <ralinesilva1@gmail.com>
2021-12-22 15:54:45 -0300

    modelar model endereco

---
+++ src/main/java/br/com/zup/Cadastro/Endereco/endereco/endereco.java
@@ -0,0 +1,33 @@
+package br.com.zup.Cadastro.Endereco.endereco;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table (name = "enderecos")
+@Data
+public class endereco {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank
+    private String cep;
+    @NotBlank
+    private String logradouro;
+    @NotBlank
+    private int numero;
+    private String complemento;
+    @NotBlank
+    private String bairro;
+    @NotBlank
+    private String cidade;
+    @NotBlank
+    private String estado;
+
+}
