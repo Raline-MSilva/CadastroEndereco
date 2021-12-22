3c7a81bf545ead2d6c59fa6a39d27f04a21f4883
Raline Silva <ralinesilva1@gmail.com>
2021-12-22 15:38:08 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/Cadastro/Endereco/CadastroEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.Cadastro.Endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEnderecoApplication.class, args);
+	}
+
+}
