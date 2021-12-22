77c2af3e629d6119da9349f14843251de067e0e5
Raline Silva <ralinesilva1@gmail.com>
2021-12-22 16:00:35 -0300

    remover anotacao

--- src/main/java/br/com/zup/Cadastro/Endereco/endereco/Endereco.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/endereco/Endereco.java
@@ -7,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table (name = "enderecos")
@@ -16,18 +15,12 @@ public class Endereco {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
-    @NotBlank
     private String cep;
-    @NotBlank
     private String logradouro;
-    @NotBlank
     private int numero;
     private String complemento;
-    @NotBlank
     private String bairro;
-    @NotBlank
     private String cidade;
-    @NotBlank
     private String estado;
 
 }
