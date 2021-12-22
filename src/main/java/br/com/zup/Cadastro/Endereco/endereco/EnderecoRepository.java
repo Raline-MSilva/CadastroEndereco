73d29749eebdac5f8f330217a7931f4226b96dfe
Raline Silva <ralinesilva1@gmail.com>
2021-12-22 16:42:31 -0300

    criar repository

---
+++ src/main/java/br/com/zup/Cadastro/Endereco/endereco/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.Cadastro.Endereco.endereco;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository <Endereco, Integer> {
+
+}
