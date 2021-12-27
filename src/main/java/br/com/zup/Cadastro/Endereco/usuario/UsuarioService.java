b6ae75c1b13ff771cadb4a70eb479e84aaed59c0
Raline Silva <ralinesilva1@gmail.com>
2021-12-27 14:35:46 -0300

    acrescentar condicao para cpf

--- src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioService.java
@@ -1,6 +1,6 @@
 package br.com.zup.Cadastro.Endereco.usuario;
 
-import br.com.zup.Cadastro.Endereco.Excecoes.EmailJaCadastradoException;
+import br.com.zup.Cadastro.Endereco.Excecoes.UuarioJaCadastradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +10,10 @@ public class UsuarioService {
     private UsuarioRepository usuarioRepository;
 
     public Usuario salvar(Usuario usuario){
-        if (usuarioRepository.existsByEmail(usuario.getEmail())) {
-            throw new EmailJaCadastradoException("Email já cadastrado");
+        if (usuarioRepository.existsByEmail(usuario.getEmail()) || usuarioRepository.existsByCpf(usuario.getCpf())) {
+            throw new UuarioJaCadastradoException("usuario ja cadastrado");
         }
+
         return usuarioRepository.save(usuario);
     }
 
