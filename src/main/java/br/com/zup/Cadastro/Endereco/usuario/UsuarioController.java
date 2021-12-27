a14c70f07967992e883531071297e248cfe2cd43
Raline Silva <ralinesilva1@gmail.com>
2021-12-27 11:51:11 -0300

    adicionar o request body

--- src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioController.java
@@ -1,8 +1,13 @@
 package br.com.zup.Cadastro.Endereco.usuario;
 
+import br.com.zup.Cadastro.Endereco.dtos.UsuarioEntradaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +18,10 @@ public class UsuarioController {
     @Autowired
     private ModelMapper modelMapper;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsuarioEntradaDTO cadastrar (@RequestBody UsuarioEntradaDTO usuarioEntradaDTO){
+        Usuario usuario = modelMapper.map(usuarioEntradaDTO, Usuario.class);
+        return modelMapper.map(usuarioService.salvar(usuario), UsuarioEntradaDTO.class);
+    }
 }
