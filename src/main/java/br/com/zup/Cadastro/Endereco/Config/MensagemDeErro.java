88c92766fe027f5a4654e401fe062722339afd9a
Raline Silva <ralinesilva1@gmail.com>
2021-12-27 12:14:41 -0300

    criar controller advice

---
+++ src/main/java/br/com/zup/Cadastro/Endereco/Config/MensagemDeErro.java
@@ -0,0 +1,9 @@
+package br.com.zup.Cadastro.Endereco.Config;
+
+public class MensagemDeErro {
+    private String mensagem;
+
+    public MensagemDeErro(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
