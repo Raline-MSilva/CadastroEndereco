4dd38a10617d908e01592452e63ee8b2f80c60f8
Raline Silva <ralinesilva1@gmail.com>
2021-12-27 12:15:30 -0300

    criar excecao para email ja cadastrado

---
+++ src/main/java/br/com/zup/Cadastro/Endereco/Excecoes/EmailJaCadastradoException.java
@@ -0,0 +1,7 @@
+package br.com.zup.Cadastro.Endereco.Excecoes;
+
+public class EmailJaCadastradoException extends RuntimeException{
+    public EmailJaCadastradoException(String message) {
+        super(message);
+    }
+}
