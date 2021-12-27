28ec1129799c8e67cc5bcbd43afe3fc2290883a2
Raline Silva <ralinesilva1@gmail.com>
2021-12-27 11:32:43 -0300

    criar classe usuario dto

---
+++ src/main/java/br/com/zup/Cadastro/Endereco/dtos/UsuarioEntradaDTO.java
@@ -0,0 +1,11 @@
+package br.com.zup.Cadastro.Endereco.dtos;
+
+
+import lombok.Data;
+
+@Data
+public class UsuarioEntradaDTO {
+    private String nome;
+    private String cpf;
+    private String email;
+}
