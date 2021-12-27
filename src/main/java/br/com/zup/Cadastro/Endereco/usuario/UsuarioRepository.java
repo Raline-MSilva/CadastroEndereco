8a3c791c71b4d37caa4a13d0369a17fefb6f9d48
Raline Silva <ralinesilva1@gmail.com>
2021-12-27 14:34:34 -0300

    criar uma query para filtro

--- src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
 
     boolean existsByEmail(String email);
+    boolean existsByCpf(String cpf);
 }
