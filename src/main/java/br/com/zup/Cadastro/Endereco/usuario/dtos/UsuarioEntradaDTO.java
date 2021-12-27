67667878055a9934847005dfdb2b64c6df4e69fc
Raline Silva <ralinesilva1@gmail.com>
2021-12-27 12:18:55 -0300

    adicionar anotacao de validacao

--- src/main/java/br/com/zup/Cadastro/Endereco/usuario/dtos/UsuarioEntradaDTO.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/usuario/dtos/UsuarioEntradaDTO.java
@@ -1,11 +1,18 @@
-package br.com.zup.Cadastro.Endereco.dtos;
+package br.com.zup.Cadastro.Endereco.usuario.dtos;
 
 
 import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class UsuarioEntradaDTO {
+    @NotBlank
     private String nome;
+    @CPF
     private String cpf;
+    @Email
     private String email;
 }
