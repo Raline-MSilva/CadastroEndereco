f2b71d4386e9c8c5f4bacea5481a31645e8487a6
Raline Silva <ralinesilva1@gmail.com>
2021-12-28 16:11:23 -0300

    criar endereco dto para exibir

---
+++ src/main/java/br/com/zup/Cadastro/Endereco/endereco/dtos/EnderecoExibirDTO.java
@@ -0,0 +1,14 @@
+package br.com.zup.Cadastro.Endereco.endereco.dtos;
+
+import lombok.Data;
+
+@Data
+public class EnderecoExibirDTO {
+    private String cep;
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+}
