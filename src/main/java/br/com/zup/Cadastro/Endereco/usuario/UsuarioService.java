14a74e73d03ec0c80421fa3f3b015176828a08bc
Raline Silva <ralinesilva1@gmail.com>
2021-12-28 14:34:03 -0300

    buscar usuario por id

--- src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioService.java
@@ -1,8 +1,12 @@
 package br.com.zup.Cadastro.Endereco.usuario;
 
-import br.com.zup.Cadastro.Endereco.Excecoes.UuarioJaCadastradoException;
+import br.com.zup.Cadastro.Endereco.Excecoes.UsuarioNaoEncontradoException;
+import br.com.zup.Cadastro.Endereco.Excecoes.UsuarioJaCadastradoException;
+import br.com.zup.Cadastro.Endereco.usuario.dtos.UsuarioSaidaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -11,10 +15,26 @@ public class UsuarioService {
 
     public Usuario salvar(Usuario usuario){
         if (usuarioRepository.existsByEmail(usuario.getEmail()) || usuarioRepository.existsByCpf(usuario.getCpf())) {
-            throw new UuarioJaCadastradoException("usuario ja cadastrado");
+            throw new UsuarioJaCadastradoException("usuario ja cadastrado");
         }
 
         return usuarioRepository.save(usuario);
     }
 
+    public Usuario buscarUsuarioPorId (int id){
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isEmpty()){
+            throw new UsuarioNaoEncontradoException("Usuario não encontrado");
+        }
+
+        return usuario.get();
+    }
+
+    public UsuarioSaidaDTO converterUsuarioDto (Usuario usuario){
+        UsuarioSaidaDTO usuarioDto = new UsuarioSaidaDTO();
+        usuarioDto.setCpf(usuario.getCpf());
+        usuarioDto.setEmail(usuario.getEmail());
+        usuarioDto.setNome(usuario.getNome());
+        return usuarioDto;
+    }
 }
