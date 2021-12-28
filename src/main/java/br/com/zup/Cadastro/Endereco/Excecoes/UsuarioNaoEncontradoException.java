547878831d30f887fc998e9b61fd4eddfbc5557e
Raline Silva <ralinesilva1@gmail.com>
2021-12-28 09:26:22 -0300

    mudar nome da classe

--- src/main/java/br/com/zup/Cadastro/Endereco/Excecoes/UsuarioNaoEncontradoException.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/Excecoes/UsuarioNaoEncontradoException.java
@@ -1,7 +1,7 @@
 package br.com.zup.Cadastro.Endereco.Excecoes;
 
-public class UsuarioNaoEncontrado extends RuntimeException{
-    public UsuarioNaoEncontrado(String message) {
+public class UsuarioNaoEncontradoException extends RuntimeException{
+    public UsuarioNaoEncontradoException(String message) {
         super(message);
     }
 }
