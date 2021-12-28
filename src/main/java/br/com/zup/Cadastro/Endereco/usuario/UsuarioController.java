596669a3abab75c68a23197f23ad66f80eca8ad1
Raline Silva <ralinesilva1@gmail.com>
2021-12-28 18:20:49 -0300

    exibir usuarios

--- src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioController.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioController.java
@@ -1,10 +1,13 @@
 package br.com.zup.Cadastro.Endereco.usuario;
 
+import br.com.zup.Cadastro.Endereco.endereco.Endereco;
+import br.com.zup.Cadastro.Endereco.endereco.dtos.EnderecoExibirDTO;
 import br.com.zup.Cadastro.Endereco.usuario.dtos.UsuarioEntradaDTO;
 import br.com.zup.Cadastro.Endereco.usuario.dtos.UsuarioSaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping ("/usuarios")
+@RequestMapping("/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
@@ -23,8 +28,18 @@ public class UsuarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UsuarioSaidaDTO cadastrar (@RequestBody @Valid UsuarioEntradaDTO usuarioEntradaDTO){
+    public UsuarioSaidaDTO cadastrar(@RequestBody @Valid UsuarioEntradaDTO usuarioEntradaDTO) {
         Usuario usuario = modelMapper.map(usuarioEntradaDTO, Usuario.class);
         return modelMapper.map(usuarioService.salvar(usuario), UsuarioSaidaDTO.class);
+    }
+
+    @GetMapping
+    public List<UsuarioSaidaDTO> exibirUsuarios() {
+        List<UsuarioSaidaDTO> listarUsuarios = new ArrayList<>();
+        for (Usuario usuarios : usuarioService.exibir()) {
+            UsuarioSaidaDTO usuarioSaidaDTO = modelMapper.map(usuarios, UsuarioSaidaDTO.class);
+            listarUsuarios.add(usuarioSaidaDTO);
+        }
+        return listarUsuarios;
     }
 }
--- src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioService.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioService.java
@@ -2,10 +2,12 @@ package br.com.zup.Cadastro.Endereco.usuario;
 
 import br.com.zup.Cadastro.Endereco.Excecoes.UsuarioNaoEncontradoException;
 import br.com.zup.Cadastro.Endereco.Excecoes.UsuarioJaCadastradoException;
+import br.com.zup.Cadastro.Endereco.endereco.Endereco;
 import br.com.zup.Cadastro.Endereco.usuario.dtos.UsuarioSaidaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,7 +15,7 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    public Usuario salvar(Usuario usuario){
+    public Usuario salvar(Usuario usuario) {
         if (usuarioRepository.existsByEmail(usuario.getEmail()) || usuarioRepository.existsByCpf(usuario.getCpf())) {
             throw new UsuarioJaCadastradoException("usuario ja cadastrado");
         }
@@ -21,20 +23,17 @@ public class UsuarioService {
         return usuarioRepository.save(usuario);
     }
 
-    public Usuario buscarUsuarioPorId (int id){
+    public Usuario buscarUsuarioPorId(int id) {
         Optional<Usuario> usuario = usuarioRepository.findById(id);
-        if (usuario.isEmpty()){
+        if (usuario.isEmpty()) {
             throw new UsuarioNaoEncontradoException("Usuario não encontrado");
         }
 
         return usuario.get();
     }
 
-    public UsuarioSaidaDTO converterUsuarioDto (Usuario usuario){
-        UsuarioSaidaDTO usuarioDto = new UsuarioSaidaDTO();
-        usuarioDto.setCpf(usuario.getCpf());
-        usuarioDto.setEmail(usuario.getEmail());
-        usuarioDto.setNome(usuario.getNome());
-        return usuarioDto;
+    public List<Usuario> exibir (){
+        List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
+        return usuarios;
     }
 }
