a1f78815ade1fa26f7b82772cfd87fc78447ee7d
Raline Silva <ralinesilva1@gmail.com>
2021-12-28 14:28:10 -0300

    cadastrar endereco

--- src/main/java/br/com/zup/Cadastro/Endereco/endereco/EnderecoController.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/endereco/EnderecoController.java
@@ -1,8 +1,14 @@
 package br.com.zup.Cadastro.Endereco.endereco;
 
+import br.com.zup.Cadastro.Endereco.endereco.dtos.EnderecoEntradaDTO;
+import br.com.zup.Cadastro.Endereco.endereco.dtos.EnderecoSaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +18,11 @@ public class EnderecoController {
     private EnderecoService enderecoService;
     @Autowired
     private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus (HttpStatus.CREATED)
+    public EnderecoSaidaDTO cadastrar (@RequestBody EnderecoEntradaDTO enderecoEntradaDTO){
+        Endereco endereco = enderecoService.converterEntity(enderecoEntradaDTO);
+        return enderecoService.converterDto(enderecoService.salvar(endereco));
+    }
 }
