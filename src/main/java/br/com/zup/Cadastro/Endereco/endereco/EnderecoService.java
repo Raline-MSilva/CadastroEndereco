61759c550fcd0623ebe6f423915bb9252d3c1840
Raline Silva <ralinesilva1@gmail.com>
2021-12-28 14:30:14 -0300

    criar conversores para dto

--- src/main/java/br/com/zup/Cadastro/Endereco/endereco/EnderecoService.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/endereco/EnderecoService.java
@@ -1,5 +1,9 @@
 package br.com.zup.Cadastro.Endereco.endereco;
 
+import br.com.zup.Cadastro.Endereco.endereco.dtos.EnderecoEntradaDTO;
+import br.com.zup.Cadastro.Endereco.endereco.dtos.EnderecoSaidaDTO;
+import br.com.zup.Cadastro.Endereco.usuario.Usuario;
+import br.com.zup.Cadastro.Endereco.usuario.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +11,41 @@ import org.springframework.stereotype.Service;
 public class EnderecoService {
     @Autowired
     private EnderecoRepository enderecoRepository;
+    @Autowired
+    private UsuarioService usuarioService;
+
+    public Endereco salvar(Endereco endereco) {
+        usuarioService.buscarUsuarioPorId(endereco.getUsuario().getId());
+        return enderecoRepository.save(endereco);
+    }
+
+    public EnderecoSaidaDTO converterDto(Endereco endereco) {
+        EnderecoSaidaDTO enderecoSaidaDTODTO = new EnderecoSaidaDTO();
+        enderecoSaidaDTODTO.setCep(endereco.getCep());
+        enderecoSaidaDTODTO.setLogradouro(endereco.getLogradouro());
+        enderecoSaidaDTODTO.setNumero(endereco.getNumero());
+        enderecoSaidaDTODTO.setComplemento(endereco.getComplemento());
+        enderecoSaidaDTODTO.setBairro(endereco.getBairro());
+        enderecoSaidaDTODTO.setCidade(endereco.getCidade());
+        enderecoSaidaDTODTO.setEstado(endereco.getEstado());
+        Usuario usuario = usuarioService.buscarUsuarioPorId(endereco.getUsuario().getId());
+        enderecoSaidaDTODTO.setUsuario(usuarioService.converterUsuarioDto(usuario));
+        return enderecoSaidaDTODTO;
+    }
+
+    public Endereco converterEntity(EnderecoEntradaDTO enderecoEntradaDTO) {
+        Endereco endereco = new Endereco();
+        endereco.setCep(enderecoEntradaDTO.getCep());
+        endereco.setLogradouro(enderecoEntradaDTO.getLogradouro());
+        endereco.setNumero(enderecoEntradaDTO.getNumero());
+        endereco.setComplemento(enderecoEntradaDTO.getComplemento());
+        endereco.setBairro(enderecoEntradaDTO.getBairro());
+        endereco.setCidade(enderecoEntradaDTO.getCidade());
+        endereco.setEstado(enderecoEntradaDTO.getEstado());
+        Usuario usuario = usuarioService.buscarUsuarioPorId(enderecoEntradaDTO.getUsuarioId());
+        endereco.setUsuario(usuario);
+        return endereco;
+    }
+
+
 }
