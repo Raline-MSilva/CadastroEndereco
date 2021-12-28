c2b5d753430bc7f8ad8ab164a963de58766308b3
Raline Silva <ralinesilva1@gmail.com>
2021-12-28 14:31:07 -0300

    adicionar anotacoes

--- src/main/java/br/com/zup/Cadastro/Endereco/endereco/dtos/EnderecoEntradaDTO.java
+++ src/main/java/br/com/zup/Cadastro/Endereco/endereco/dtos/EnderecoEntradaDTO.java
@@ -1,16 +1,24 @@
 package br.com.zup.Cadastro.Endereco.endereco.dtos;
 
-import br.com.zup.Cadastro.Endereco.usuario.Usuario;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class EnderecoEntradaDTO {
+    @NotBlank
     private String cep;
+    @NotBlank
     private String logradouro;
+    @NotBlank
     private int numero;
     private String complemento;
+    @NotBlank
     private String bairro;
+    @NotBlank
     private String cidade;
+    @NotBlank
     private String estado;
-    private Usuario usuario;
+    @NotBlank
+    private int usuarioId;
 }
