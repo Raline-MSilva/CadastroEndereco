55fdc540ec081f9419d7ddb4efca906980cd9e26
Raline Silva <ralinesilva1@gmail.com>
2021-12-29 12:10:10 -0300

    testar cadastrar usuario na service

---
+++ src/test/java/br/com/zup/Cadastro/Endereco/usuario/UsuarioServiceTest.java
@@ -0,0 +1,44 @@
+package br.com.zup.Cadastro.Endereco.usuario;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class UsuarioServiceTest {
+    @MockBean
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    private Usuario usuario;
+    List<Usuario> usuarios;
+
+    @BeforeEach
+    public void setup(){
+        usuario = new Usuario();
+        usuario.setNome("Maria");
+        usuario.setCpf("094.408.150-94");
+        usuario.setEmail("maria@maria");
+
+        usuarios = new ArrayList<>();
+    }
+
+    @Test
+    public void testarCadastrarUsuario(){
+        Mockito.when(usuarioRepository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+        Mockito.when(usuarioRepository.existsByEmail(usuario.getEmail())).thenReturn(false);
+        Mockito.when(usuarioRepository.existsByCpf(usuario.getCpf())).thenReturn(false);
+        usuarioService.salvar(usuario);
+        Mockito.verify(usuarioRepository, Mockito.times(1)).save(Mockito.any(Usuario.class));
+        Mockito.verify(usuarioRepository, Mockito.times(1)).existsByEmail(usuario.getEmail());
+        Mockito.verify(usuarioRepository, Mockito.times(1)).existsByCpf(usuario.getCpf());
+    }
+}
